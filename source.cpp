ad3c11c3ec8d998590b3b49b9495f52ad4f58914
deadyzs003 <deadyzs003@gmail.com>
2021-11-09 10:41:18 +0700

    da them chuc nang cong

--- source.cpp
+++ source.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
-int tong(int a, int b);
+int tong(int a, int b)
+{
+	return a+b;
+}
 int hieu(int a, int b);
 
 int main()
@@ -12,7 +15,9 @@ int main()
 	cout << "Nhap so nguyen 2: ";
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
-	cin >> phepToan;
+	cin >> phepToan
+	if(phepToan=='+')
+		cout << "Tong 2 so la: "<<tong(a,b)<<endl;
 	system("pause");
 	return 0;
 }
